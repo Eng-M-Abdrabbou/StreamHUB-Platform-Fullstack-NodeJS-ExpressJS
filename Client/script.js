793a35a8324a378b44b64f6980b36d11f377cbec
Eng-M-Abdrabbou <mahmoud.abdrabbou.23@gmail.com>
2024-11-24 15:45:36 +0400

    removed some comments and deleted some unnecessary files

--- Client/script.js
+++ Client/script.js
@@ -124,7 +124,7 @@ video.addEventListener("timeupdate", () => {
   timelineContainer.style.setProperty("--progress-position", percent)
 })
 
-const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+const leadZeroFormat = new Intl.NumberFormat(undefined, {
   minimumIntegerDigits: 2,
 })
 function formatDuration(time) {
@@ -132,11 +132,11 @@ function formatDuration(time) {
   const minutes = Math.floor(time / 60) % 60
   const hours = Math.floor(time / 3600)
   if (hours === 0) {
-    return `${minutes}:${leadingZeroFormatter.format(seconds)}`
+    return `${minutes}:${leadZeroFormat.format(seconds)}`
   } else {
-    return `${hours}:${leadingZeroFormatter.format(
+    return `${hours}:${leadZeroFormat.format(
       minutes
-    )}:${leadingZeroFormatter.format(seconds)}`
+    )}:${leadZeroFormat.format(seconds)}`
   }
 }
 
